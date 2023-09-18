bcebb1c4ff78de787b2a6d032ca50ac18d25cc57
Andreas Streichardt <andreas@mop.koeln>
2023-09-18 08:20:21 +0200

    ✨ post drink to mastodon

---
+++ src/routers/meteApiUsersRouter.ts
@@ -0,0 +1,67 @@
+import { Request, Response, Router } from "express";
+import asyncRequestHandler from "../utils/asyncHandler";
+import fetch from "node-fetch";
+import { createRestAPIClient } from "masto";
+
+if (!process.env.METE_BASEURL) {
+  throw new Error("METE_BASEURL not set");
+}
+
+let meteBaseUrl = process.env.METE_BASEURL;
+if (meteBaseUrl.endsWith("/")) {
+  meteBaseUrl = meteBaseUrl.slice(-1);
+}
+
+let masto;
+if (process.env.MASTODON_ACCESS_TOKEN) {
+  masto = createRestAPIClient({
+    url: "https://botsin.space",
+    accessToken: process.env.MASTODON_ACCESS_TOKEN,
+  });
+}
+
+const postDrinkToMastodon = async (drinkId: number) => {
+  if (!masto) {
+    console.warn(
+      "Not posting to mastodon because MASTODON_ACCESS_TOKEN not set"
+    );
+    return;
+  }
+
+  const drinkResponse = await fetch(
+    `${meteBaseUrl}/api/v1/drinks/${drinkId}.json`
+  );
+  if (drinkResponse.status !== 200) {
+    throw new Error(
+      `Fetching drink has unexpected status code ${drinkResponse.status}`
+    );
+  }
+  const drink = await drinkResponse.json();
+  await masto.v1.statuses.create({
+    status: `${drink.name} wurde gekauft!`,
+    visibility: "public",
+  });
+};
+
+const router = Router();
+router.get(
+  "/:id/buy.json",
+  asyncRequestHandler(async (req: Request, res: Response) => {
+    const { drink: drinkId } = req.query;
+    // originalUrl contains query as well
+    const buyUrl = meteBaseUrl + req.originalUrl;
+    const meteResponse = await fetch(buyUrl);
+    if (meteResponse.status !== 204) {
+      throw new Error(
+        `mete response status is ${meteResponse.status} which is unexpected. something is wrong`
+      );
+    }
+    res.status(204);
+    // finish request for user but continue posting mastodon stuff in the background
+    res.end();
+
+    await postDrinkToMastodon(parseInt(drinkId as string, 10));
+  })
+);
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import * as express from "express";
 import sequelize from "./db";
 import userMappingRouter from "./routers/userMappingRouter";
+import meteApiUsersRouter from "./routers/meteApiUsersRouter";
 
 const port = process.env.PORT ? process.env.PORT : 3003;
 
 const app = express();
 app.use(express.json());
 app.use("/user-mapping/", userMappingRouter);
+app.use("/api/v1/users", meteApiUsersRouter);
 
 sequelize.sync({}).then(() => {
   app.listen(port, () => {
